32ba10223d926e87a49e702c053cdd6d2af9ec45
HadenBennett4 <43655146+HadenBennett4@users.noreply.github.com>
2021-03-31 10:49:03 -0500

    Updated checkoutpage

---
+++ src/components/checkoutBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ name }}</h5>
+      <p class="card-text">
+          {{price}}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    name: String,
+    price: String
+  }
+}
+</script>
+
+<style>
+  .card {
+    margin: 10px;
+  }
+
+</style>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,8 +1,24 @@
 <template> 
   <h1>Checkout</h1>
-    <li v-for="item in this.$store.state.order" v-bind:key="item"> {{item}}</li>
+    <div class="container">
+    <div class="row">
+      <div v-for="item in this.$store.state.order" v-bind:key="item" class="col-12">
+          <checkoutBox :name="item" :price="item" />
+      </div>
+    </div>
+  </div>
 </template>
+
 <script>
+import checkoutBox from '@/components/checkoutBox.vue'
+export default {
+  components: { checkoutBox },
+  computer: {
+    users() {
+      return this.$store.state.order;
+    }
+  },
+}
 </script>
 
 <style>
