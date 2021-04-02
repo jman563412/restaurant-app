5d827518584b5b9f2d278f3f214a61f34da75376
HadenBennett4 <43655146+HadenBennett4@users.noreply.github.com>
2021-04-02 12:21:03 -0500

    Added Payment Complete Page

--- src/views/paymentComplete.vue
+++ src/views/paymentComplete.vue
@@ -1,6 +1,18 @@
 <template>
-    
-    ORDER COMPLETE
+    <body class="backgroundcolor">
+    <font size="+12">PAYMENT COMPLETE</font>
+    <p>
+        <font size="+2">While you wait for your food please feel free <br>to play any of our games or use the music tab!!!</font>
+        </p>
     <br>
-    Review Debug - {{this.$store.state.review}}
+    </body>
 </template>
+<style>
+    .backgroundcolor {
+        background-color: rgb(233, 161, 67);
+        text-align: center;
+        padding-bottom: 500px;
+        padding-top: 10%;
+        height: 100%,
+    }
+</style>
