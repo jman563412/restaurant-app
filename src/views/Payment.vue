a9d753897b490a9f18eb0d5e611e5c8f9441cbbe
mario0220 <80548453+mario0220@users.noreply.github.com>
2021-04-02 10:55:27 -0500

    Update Payment.vue

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>Payment</h1>
+  <div class="ReviewBox">  
+    <label >Write a Review:</label>
+    <br>
+    <textarea rows="10" cols="100">
+      </textarea>
+      <br>
+      <button class="submit">submit</button>
+  </div>
+
 </template>
 
 <script>
@@ -9,5 +18,10 @@ export default {
 </script>
 
 <style>
-
+.ReviewBox{
+  margin: 100px;
+}
+.submit{
+    background-color: #ff0000;
+}
 </style>
